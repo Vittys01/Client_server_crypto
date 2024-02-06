3648cf43cd804e4348ee3be90ea3839b50a55cbc
Vittys01 <miguelbernaleewq@gmail.com>
2024-02-06 10:01:05 -0500

    server to server to pair keys

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-import dotenv from "dotenv";
 import crypto from "crypto";
-import {generateKeyPair} from "node-jose/lib/deps/ecc/index.js";
-
+import fs from "fs";
+import dotenv from "dotenv";
 dotenv.config();
-
-const generateKey = async () => {
-    return crypto.subtle.generateKey({ name: "AES-CBC", length: 192 }, true, [
-        "encrypt",
-        "decrypt",
-    ]);
-};
-function hexStringToArrayBuffer(hexString) {
-    // remove the leading 0x
-    hexString = hexString.replace(/^0x/, "");
-
-    // ensure even number of characters
-    if (hexString.length % 2 != 0) {
-        console.log(
-            "WARNING: expecting an even number of characters in the hexString"
-        );
-    }
-
-    // create an array buffer from the hex string
-    const arrayBuffer = new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16))).buffer;
-
-    return arrayBuffer;
-}
 
 const importKey = async (keyBuffer) => {
     return crypto.subtle.importKey("raw", keyBuffer, {name: "AES-CBC"}, true, [
@@ -54,22 +30,37 @@ const buf2hex = (buffer) => {
       .map((x) => x.toString(16).padStart(2, "0"))
       .join("");
 };
-const exportKey = async (key) => {
-    return crypto.subtle.exportKey("raw", key);
-};
+/*
 (async function () {
-
-    const a = await importKey(stringToArrayBuffer("72f402c3-3a99-432d-9d61-5ba6db8b48e8".replace(/-/g, "")));
-    console.log(a )
-    /*
+    //PUBLIC
     const importedKey = await importKey( stringToArrayBuffer("e8c40a8d-4fba-4afb-bee5-0029141775dc".replace(/-/g, "")));
     let data = {
         "id": "e8c40a8d-4fba-4afb-bee5-0029141775dc",
-        "name": "John Doe",
-        "age": 25
+        "name": "John Doea",
     };
     const encryptedData = await encrypt(JSON.stringify(data), importedKey);
     console.log("Encrypted data:", buf2hex(encryptedData.data));
     console.log("IV:", buf2hex(encryptedData.iv));
-    */
+
 })()
+*/
+//SERVER A
+
+// Leer la clave pública del Servidor B
+const publicKeyB = fs.readFileSync('publicKeyB.pem', 'utf-8');
+
+// Objeto JSON a cifrar
+const originalObject = {
+    uuid:"e8c40a8d-4fba-4afb-bee5-0029141775dc",
+    fuction:"susbtract money",
+};
+
+// Cifrado asimétrico usando la clave pública
+const encryptedBuffer = crypto.publicEncrypt({
+    key: publicKeyB,
+    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+    oaepHash: 'sha256',
+}, Buffer.from(JSON.stringify(originalObject), 'utf-8'));
+
+fs.writeFileSync('encryptedMessageFromA.txt', encryptedBuffer, 'utf-8');
+console.log('Objeto cifrado desde Servidor A:\n', encryptedBuffer.toString('base64'));
